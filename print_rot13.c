28de030d19e0985e62dede00b2d64352a9c672b7
Armynerh <aimenah@outlook.com>
2021-10-08 12:40:28 -0700

    Added the Rot13

---
+++ print_rot13.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * print_rot13 - a function that encodes a string using rot13 and
+ * prrint to stdout
+ *
+ * @list: A list of argument of va_list type from stdarg.h
+ * Return: void
+ */
+int print_rot13(va_list list)
+{
+	char *s = va_arg(list, char*);
+	int i, j, count;
+	char *a = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char *b = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	count = 0;
+	for (i = 0; *(s + i); i++)
+	{
+		for (j = 0; *(a + j); j++)
+		{
+			if (*(s + i) == *(a + j))
+			{
+				count += write(1, (b + j), 1);
+				break;
+			}
+		}
+
+		return (count);
+}
