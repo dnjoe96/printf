ae5f65307bac9d57591dc21dca3dd04cd665c5e1
dnjoe96 <donjoedbest@gmail.com>
2021-10-08 16:03:24 +0100

    Fixed Errors in the _printf.c function

--- _printf.c
+++ _printf.c
@@ -10,15 +10,15 @@
  */
 int _printf(const char *format, ...)
 {
-	int i, j, count;
+	int i, j, count, find;
 	va_list list;
-	set arguments [] = {
+	set arguments[] = {
 		{'c', print_char},
 		{'d', print_d},
 		{'i', print_d},
 		{'s', print_str},
 	};
-	
+
 	if (format == NULL)
 		return (-1);
 
@@ -30,22 +30,32 @@ int _printf(const char *format, ...)
 	{
 		if (*(format + i) == '%')
 		{
-			if (*(format + i + 1) == '%')
-			{
-				count += write(1, "%", 1);
-				format++;
+			if (*(format + i + 1) == '\0')
 				continue;
-			}
+			find = 0;
 
 			for (j = 0; j < 4; j++)
 			{
 				if (*(format + i + 1) == arguments[j].spec)
 				{
 					count += arguments[j].print(list);
+					find = 1;
+					format++;
 					break;
 				}
 			}
-			format++;
+
+			if (find != 1)
+			{
+				if (*(format + i + 1) == '%')
+				{
+					count += write(1, "%", 1);
+					format++;
+				}
+				else
+					count += write(1, (format + i), 1);
+			}
+
 		}
 		else
 		{
