e19daf09542fec3de22f4ae8af506ac408fa4b3a
dnjoe96 <donjoedbest@gmail.com>
2021-10-08 12:39:56 +0100

    Modified the various print functions

--- print_d.c
+++ print_d.c
@@ -3,17 +3,22 @@
 /**
  * print_d - print integar to stdout
  *
- * @a: Integar number
- * Return: Int
+ * @list: A variable list of va_list type from stdarg.h
+ * Return: Int - The number of characters printed
  */
-int print_d(int a)
+int print_d(va_list list)
 {
-	unsigned int aux_Num, count_Zero;
+	unsigned int aux_Num, count_Zero, count, i;
+	int a;
 
+	a = va_arg(list, int);
+
+	count = 0;
 	if (a < 0)
 	{
 		a = a * -1;
-		_putchar(45);
+		i = 45;
+		count += write(1, &i, 1);
 	}
 
 	aux_Num = a;
@@ -27,8 +32,9 @@ int print_d(int a)
 
 	while (count_Zero >= 1)
 	{
-		_putchar(((a / count_Zero) % 10) + '0');
+		i = ((a / count_Zero) % 10) + 48;
+		count += write(1, &i, 1);
 		count_Zero /= 10;
 	}
-	return (0);
+	return (count);
 }
--- print_str.c
+++ print_str.c
@@ -3,19 +3,26 @@
 /**
  * print_str - prints string
  *
- * @s: string input
- * Return: Int response
+ * @list: A variable list of type va_list of stdarg.h
+ * Return: Int - Number of characters printed
  */
-int print_str(char *s)
+int print_str(va_list list)
 {
+	int count;
+	char *s;
+
+	s = va_arg(list, char*);
+
 	if (*s == '\0')
 		return (0);
 
+	count = 0;
+
 	while (*s)
 	{
-		_putchar(*s);
+		count += write(1, s, 1);
 		s++;
 	}
 
-	return (0);
+	return (count);
 }
