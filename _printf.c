8171a53de589823bd45f31d8e155ed0a18839366
Armynerh <aimenah@outlook.com>
2021-10-08 12:32:07 +0100

    Modified new project

--- _printf.c
+++ _printf.c
@@ -1,67 +1,58 @@
 #include <stdarg.h>
+#include <unistd.h>
 #include "main.h"
 
 /**
  * _printf - printf function
  *
  * @format: formatted string
- * Return: Always 0
+ * Return: The total number of outputted characters
  */
 int _printf(const char *format, ...)
 {
-	const char *traverse;
+	int i, j, count;
 	va_list list;
+	set arguments [] = {
+		{'c', print_char},
+		{'d', print_d},
+		{'i', print_d},
+		{'s', print_str},
+	};
+	
+	if (format == NULL)
+		return (-1);
 
 	va_start(list, format);
 
-	for (traverse = format; *traverse != '\0'; traverse++)
-	{
-		while (*traverse != '%')
-		{
-			if (*traverse == '\0')
-			{
-				return (0);
-			}
-			_putchar(*traverse);
-			traverse++;
-		}
+	count = 0;
 
-		traverse++;
-		switch (*traverse)
+	for (i = 0; *(format + i); i++)
+	{
+		if (*(format + i) == '%')
 		{
-		case 'c':
-			print_char(va_arg(list, int));
-			break;
-		case 'i':
-			print_d(va_arg(list, int));
-			break;
-		case 'd':
-			print_d(va_arg(list, int));
-			break;
-		case 'b':
-			break;
-		case 's':
-			print_str(va_arg(list, char*));
-			break;
-		case 'u':
-			break;
-		case 'o':
-			break;
-		case 'x':
-			break;
-		case 'X':
-			break;
-		case '%':
-			_putchar('%');
-			break;
-		default:
-			_putchar(*(traverse - 1));
-			_putchar(*traverse);
-			break;
+			if (*(format + i + 1) == '%')
+			{
+				count += write(1, "%", 1);
+				format++;
+				continue;
+			}
+
+			for (j = 0; j < 4; j++)
+			{
+				if (*(format + i + 1) == arguments[j].spec)
+				{
+					count += arguments[j].print(list);
+					break;
+				}
+			}
+			format++;
+		}
+		else
+		{
+			count += write(1, (format + i), 1);
 		}
 	}
-
 	va_end(list);
 
-	return (0);
+	return (count);
 }
